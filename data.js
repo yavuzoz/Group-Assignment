60ae56973471f1ac442d671027d3cff7d02272ab
Your Name <y.oooguz@gmail.com>
2022-01-26 21:07:07 +0100

    data.js is added

---
+++ data.js
@@ -0,0 +1,150 @@
+const class_a = [
+    Marco = [{
+        Matematik: 80,
+        Fizik: 50,
+        Kimya: 75,
+        Geometri: 89,
+    }],
+    Laura = [{
+        Matematik: 100,
+        Fizik: 80,
+        Kimya: 70,
+        Geometri: 98,
+    }],
+    Francesca = [{
+        Matematik: 80,
+        Fizik: 90,
+        Kimya: 100,
+        Geometri: 69,
+    }],
+    Antuan = [{
+        Matematik: 80,
+        Fizik: 79,
+        Kimya: 70,
+        Geometri: 100,
+    }],
+    Filippo = [{
+        Matematik: 95,
+        Fizik: 79,
+        Kimya: 95,
+        Geometri: 100,
+    }],
+    Lisa = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 87,
+        Geometri: 76,
+    }],
+    Fatma = [{
+        Matematik: 90,
+        Fizik: 80,
+        Kimya: 80,
+        Geometri: 100,
+    }],
+    Martin = [{
+        Matematik: 69,
+        Fizik: 80,
+        Kimya: 96,
+        Geometri: 100,
+    }],
+];
+const class_b = [
+    Hasan = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 100,
+        Geometri: 80,
+    }],
+    Stefania = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 68,
+        Geometri: 80,
+    }],
+    Kenan = [{
+        Matematik: 80,
+        Fizik: 90,
+        Kimya: 80,
+        Geometri: 100,
+    }],
+    Teo = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 59,
+        Geometri: 80,
+    }],
+    Daria = [{
+        Matematik: 90,
+        Fizik: 80,
+        Kimya: 80,
+        Geometri: 94,
+    }],
+    Cristina = [{
+        Matematik: 78,
+        Fizik: 80,
+        Kimya: 88,
+        Geometri: 100,
+    }],
+    Carmen = [{
+        Matematik: 80,
+        Fizik: 69,
+        Kimya: 80,
+        Geometri: 100,
+    }],
+    Valentina = [{
+        Matematik: 80,
+        Fizik: 58,
+        Kimya: 80,
+        Geometri: 79,
+    }],
+];
+const class_c = [
+    Namık = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 92,
+        Geometri: 80,
+    }],
+    Cindy = [{
+        Matematik: 78,
+        Fizik: 80,
+        Kimya: 68,
+        Geometri: 80,
+    }],
+    Merhavit = [{
+        Matematik: 80,
+        Fizik: 90,
+        Kimya: 80,
+        Geometri: 94,
+    }],
+    Sevgi = [{
+        Matematik: 80,
+        Fizik: 80,
+        Kimya: 59,
+        Geometri: 80,
+    }],
+    Mehmet = [{
+        Matematik: 78,
+        Fizik: 79,
+        Kimya: 80,
+        Geometri: 94,
+    }],
+    Teodor = [{
+        Matematik: 78,
+        Fizik: 80,
+        Kimya: 88,
+        Geometri: 78,
+    }],
+    Carmen = [{
+        Matematik: 80,
+        Fizik: 69,
+        Kimya: 80,
+        Geometri: 87,
+    }],
+    Murat = [{
+        Matematik: 69,
+        Fizik: 58,
+        Kimya: 80,
+        Geometri: 79,
+    }],
+];
